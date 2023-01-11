410639a76a7f247ef864a837b79fc2eb4cd7fc05
L183R <liberolascuaga@gmail.com>
2023-01-11 19:34:22 +0000

    Mi primer commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,25 +1,58 @@
-import React from "react";
+import React, {useState} from "react";
+// import Tabla from "/.Tabla.jsx";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+
+
+	//declaracion de estados
+	// espacio de memoria, la funcion que actualiza elvalor inicial
+	const[input,setInput]=useState("")//1. creamos un estado del input email
+	// const[password,setPassword]=useState("")
+	const [todos, setTodos] = useState([])
+	//3.vinculamos la funcion
+	// function handlePassword(e) {
+	// 	setPassword(e.target.value)
+	// }
+		// function removerItem(index) {
+		// 	const newLista = [...todos];
+		// 	newLista.splice(index, 1);
+		// 	setTodos(newLista);
+		// }
+		function removerItem(event) {
+			
+			let borranding = event.target.id
+			console.log(todos)
+			console.log(borranding)
+			setTodos (todos.filter(todos => todos != borranding))
+			}
+
+	// 4. procesamos todos los datos del formulario
+	function enviarDatos(e) {
+		e.preventDefault()// detenemos el comportamiento predeterminado para procesar nuestro codigo
+		setTodos([...todos, input]);
+		setInput("");
+	}
+	
+
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+		<>
+		<form className="container" onSubmit={enviarDatos}>
+  <div className="mb-3">
+    <img src="https://53.fs1.hubspotusercontent-na1.net/hub/53/hubfs/To_Do_List.png?width=893&height=600&name=To_Do_List.png"></ img>
+    {/*2. definimos el evento ochange en el input */}
+	<input type="text" className="form-control" id="elInput" aria-describedby="emailHelp" onChange={(e)=>{setInput(e.target.value)}} value={input}></ input>
+    <div id="inputList" className="form-text">
+		<ul className="list-group">
+			{todos.map((element, index) => <div className="row"><li className="list-group-item col-11">{element}</li><button type="button" className="btn btn-outline-danger col-1" onClick={() => removerItem(event)} id={element}>X</button></div>)}
+		</ul>
+	</div>
+  </div>
+</form>
+	
+</>
 	);
 };
 
